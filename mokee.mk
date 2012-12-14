8da375d4aa054466052349357a9ffada8f147423
martincz <martincz@mokeetek.com>
2012-12-14 18:18:36 +0800

    device_samsung_maguro init
    Change-Id: Iffbda39c44cc444f38d7ec1b44d1ed7af8ed6392

---
+++ mokee.mk
@@ -0,0 +1,29 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/mk/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := GN-GSM
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+## Device identifier. This must come after all inclusions
+
+PRODUCT_NAME := mk_maguro
+PRODUCT_BRAND := Google
+PRODUCT_DEVICE := maguro
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.1.2/JZO54K/485486:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.2 JZO54K 485486 release-keys"
